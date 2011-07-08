1bf90754a5965db2577e7a39d83db8b5696cefeb
iwallis <iwallis@W7LT-IWALLIS.US.THALESESEC.COM>
2011-07-08 09:37:29 -0700

    Blah

---
+++ com.ganjamob.egittest1/src/Com/ganjamob/egittest1Activity.java
@@ -0,0 +1,14 @@
+package Com.ganjamob;
+
+import com.ganjamob.egittest1.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class egittest1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
